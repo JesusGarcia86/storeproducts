a453ccc0f8e78038140d412dbb0b483dfc65fbf9
JesusGarcia86 <the_sampritas@hotmail.com>
2021-01-12 09:06:18 -0800

    first commit

---
+++ storeproducts.py
@@ -0,0 +1,23 @@
+class Store:
+    def __init__(self, store_name, products_list):
+        self.store_name = "tinystore"
+
+    def add_product(self, new_product):
+        products_list.append(new_product)
+
+    def sell_product(self, id):
+
+class Product:
+    def __init__(self, item_name, price, category):
+        self.item_name = "tennis racquet"
+        self.price = 200
+        self.category = "sports"
+
+    def update_price(self, percent_change, is_increased):
+        if self.is_increased = true
+            price += percent_change
+        elif self.is_increased = false
+            price -= percent_change
+
+    def print_info(self):
+        print(__init__)
